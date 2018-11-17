aeb40c59c93872c87ec86dbf56c1cd209c89e302
Halil Özel <halilozel1903@gmail.com>
2018-11-17 11:54:19 +0300

    Button ornegine yeni özellikler eklendi.

--- ButtonProgrammatically/ButtonProgrammatically/ViewController.swift
+++ ButtonProgrammatically/ButtonProgrammatically/ViewController.swift
@@ -16,12 +16,31 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         myButton = UIButton(type: .system) // button tipi
+        myButton.frame = CGRect(x: 77, y: 426, width: 250, height: 60)
+        myButton.center.x = self.view.center.x
+
+
         myButton.contentEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0) // içerik eki ne kadar
         myButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0) // baslik eki ne kadar
+        myButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0) // resim eki ne kadar
         
-        myButton.frame = CGRect(x: 45, y: 300, width: 280, height: 140)
+        myButton.reversesTitleShadowWhenHighlighted = false
+        myButton.adjustsImageWhenHighlighted = false
+        myButton.adjustsImageWhenDisabled = false
+        myButton.showsTouchWhenHighlighted = false
         
-        myButton.setTitle("TurkishKit", for: .normal)
+        
+        
+        
+        myButton.setTitle("TurkishKit", for: .normal) // başlık içeriği
+        myButton.setTitleColor(UIColor.red, for: .normal)  // başlığın rengi
+        myButton.setTitleShadowColor(UIColor.black, for: .normal) // başlığın gölge rengi
+        myButton.backgroundColor = UIColor.blue
+        
+       // let roundButton = UIImage(named: "btn.png")
+        
+       // myButton.setImage(roundButton, for: .normal)
+        
         
         
         
